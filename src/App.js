b16ec55a1880d627d4e610244164cf1e05fee7d9
Muhammad Hassan <hassank7766khan@gmail.com>
2025-12-23 10:45:40 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
